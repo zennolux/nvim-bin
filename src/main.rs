054ec8b1fe3a01f7b609a51cad2e94d18c167972
zennolux <zennolux@gmail.com>
2024-04-22 22:21:06 +0800

    chore: rename package name to nvimconf

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use nvim::configuration as nvimconf;
+use nvimconf::configuration;
 
 fn main() {
     println!("Started...");
-    let installed_plugin_manager = nvimconf::install(nvimconf::Kind::PluginManager).unwrap();
-    let installed_nvim_config = nvimconf::install(nvimconf::Kind::NvimConfig).unwrap();
+    let installed_plugin_manager =
+        configuration::install(configuration::Kind::PluginManager).unwrap();
+    let installed_nvim_config = configuration::install(configuration::Kind::NvimConfig).unwrap();
     println!(
         "{}\n{}\nEnjoy your neovim journey now!",
         installed_plugin_manager, installed_nvim_config
