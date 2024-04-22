69d2b85fff0382b4d343a3f0a3561c641f197413
zennolux <zennolux@gmail.com>
2024-04-22 19:41:51 +0800

    fix: print whitout symbol

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let installed_plugin_manager = nvimconf::install(nvimconf::Kind::PluginManager).unwrap();
     let installed_nvim_config = nvimconf::install(nvimconf::Kind::NvimConfig).unwrap();
     println!(
-        "{:?}\n{:?}\nEnjoy your neovim journey now!",
+        "{}\n{}\nEnjoy your neovim journey now!",
         installed_plugin_manager, installed_nvim_config
     );
 }
